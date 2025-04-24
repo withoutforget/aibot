96d98f83a79b4a14927e82f809447a7c3e677ff8
withoutforget <89633210+withoutforget@users.noreply.github.com>
2025-04-24 18:02:29 +0300

    updated reply to message-mechanism

--- routes/ai.py
+++ routes/ai.py
@@ -15,17 +15,19 @@ router.message.middleware(ChatsMiddleware())
 @router.message(Command(commands=["ai"]))
 async def generate_ai_content(message: Message, chats: Chats) -> None:
     chats.expire_chats()
-    logging.info(list(map(str, chats._chats)))
     try:
         text = message.text.lstrip('/ai ')
         if len(text) == 0:
             return 
 
 
-        if message.reply_to_message is None:
+        if message.reply_to_message is None or (
+            message.reply_to_message is not None and message.reply_to_message.text is None
+        ):
             co: ChatObject = chats.create_chat()
             chats.update_chat(co.uuid)
         else:
+            print(message.reply_to_message)
             co = chats.find_chat(message.reply_to_message.message_id)
             if co is None:
                 await message.reply('Диалог истёк. Начните заново')
